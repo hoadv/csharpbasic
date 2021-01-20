465396711903e42f528d24097ac85568fcf62c7d
Trang Nguyen <trangtnnguyen@kms.com.vn>
2021-01-20 17:25:54 +0700

    first commit

---
+++ KMS.CSharpBasic.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Humanizer;
+using System;
+
+namespace KMS.CSharpBasic.Core
+{
+    public class Class1
+    {
+        public string ConvertNumberToString(int Number) {
+            return Number.ToWords();
+        }
+    }
+}
---
+++ KMS.CSharpBasic.Overview/Program.cs
@@ -0,0 +1,15 @@
+﻿using KMS.CSharpBasic.Core;
+using System;
+
+namespace KMS.CSharpBasic.Overview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 cl = new Class1();
+            var test1 = cl.ConvertNumberToString(1);
+            Console.WriteLine(test1);
+        }
+    }
+}
