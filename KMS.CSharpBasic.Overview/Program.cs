cc641868330098cfb218f57056281240de16f23c
Hieu Trung Tran (2301) <hieutrungtran@kms.com.vn>
2021-01-22 11:07:47 +0700

    first commit

---
+++ KMS.CSharpBasic.Overview/Program.cs
@@ -0,0 +1,15 @@
+﻿using KMS.CSharpBasic.Core;
+using System;
+
+namespace KMS.CSharpBasic.Overview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Class1();
+            var word = c1.ConvertNumberToText(1);
+            Console.WriteLine(word);
+        }
+    }
+}
