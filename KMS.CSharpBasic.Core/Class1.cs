fbefee2f8be9c49625542bcf570b2b8280cc17c5
hienvnguyen <hiennv0809@gmail.com>
2021-01-23 15:04:19 +0700

    Commit BT1
    Commit BT1

---
+++ KMS.CSharpBasic.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KMS.CSharpBasic.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ KMS.CSharpBasic.Overview/Program.cs
@@ -0,0 +1,14 @@
+﻿using KMS.CSharpBasic.Core;
+using System;
+
+namespace KMS.CSharpBasic.Overview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Class1();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
