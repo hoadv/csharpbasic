9ae496876b99feee41335ac3f360d34eacfbba5d
Loan Pham <loanpham@clearwaveinc.com>
2021-01-23 22:18:24 +0700

    init project

---
+++ KMS.CSharpBasic.Core/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Humanizer;
+using System;
+
+namespace KMS.CSharpBasic.Core
+{
+    public class Class1
+    {
+        public string ConvertNumberToText(int number)
+        {
+            return number.ToWords();
+        }
+    }
+}
---
+++ KMS.CSharpBasic.Overview/Program.cs
@@ -0,0 +1,16 @@
+﻿using KMS.CSharpBasic.Core;
+using System;
+
+namespace KMS.CSharpBasic.Overview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cla = new Class1();
+            var word = cla.ConvertNumberToText(1);
+            Console.WriteLine("This is number " + word);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
