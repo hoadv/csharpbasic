2c1e428a055b865b2d8dcc0ce4dbc6a3d4694969
Minh Quach <minhquach@kms-technology.com>
2021-01-23 23:11:54 +0700

    Add session #1 code

---
+++ KMS.CSharpBasic.Core/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Humanizer;
+using System;
+
+namespace KMS.CSharpBasic.Core
+{
+    public class Class1
+    {
+        public void NumberToWord(int number)
+        {
+            Console.WriteLine(NumberToWordsExtension.ToWords(number).Pascalize());
+        }
+    }
+}
---
+++ KMS.CSharpBasic.Overview/Program.cs
@@ -0,0 +1,13 @@
+﻿using KMS.CSharpBasic.Core;
+
+namespace KMS.CSharpBasic.Overview
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.NumberToWord(10);
+        }
+    }
+}
