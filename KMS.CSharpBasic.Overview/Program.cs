44c345c1a35e76891fbef0d532c6955a581cf2a9
johnnyshi1999 <54013918+johnnyshi1999@users.noreply.github.com>
2021-01-25 09:32:37 +0700

    add core and overiview projects

---
+++ KMS.CSharpBasic.Overview/Program.cs
@@ -0,0 +1,16 @@
+﻿using KMS.CSharpBasic.Core;
+using System;
+
+namespace KMS.CSharpBasic.Overview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cl = new Class1();
+
+            string word = cl.ConvertNumberToText(1);
+            Console.WriteLine(word);
+        }
+    }
+}
