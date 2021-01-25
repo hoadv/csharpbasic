a2860c409ce5803d74d1bdec96c53a7c5802d75e
phuongtruong <phuongtruong@kms.com.vn>
2021-01-25 22:49:02 +0700

    init project

---
+++ KMS.CSharpBasic.Core/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Humanizer;
+
+namespace KMS.CSharpBasic.Core
+{
+    public class Class1
+    {
+        public string ConvertNumberToText(int number)
+        {
+            return number.ToWords();
+        }
+    }
+}
---
+++ KMS.CSharpBasic.Overview/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using KMS.CSharpBasic.Core;
+
+namespace KMS.CSharpBasic.Overview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cl = new Class1();
+            var word = cl.ConvertNumberToText(1);
+            Console.WriteLine("This is number " + word);
+        }
+    }
+}
